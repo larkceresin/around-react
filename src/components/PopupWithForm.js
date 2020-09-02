100e5857c2402fc0765d78acaf94cfd9feac6df4
Jenna Toff <63078595+larkceresin@users.noreply.github.com>
2020-09-02 09:08:11 -0700

    remove Input

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -15,13 +15,4 @@ function PopupWithForm(props){
 
 }
 
-function Input(props){
-    return <>
-        <input required id={props.name} className={`popout__form-input popout__form-input_type_${props.name}`} type="text" name={props.name} placeholder={props.name} />
-        <span id="name-error" className={`popout__form-input-error popout__form-input_type_${props.name}-error`}></span>
-  
-    </>
-}
-
-
-export {PopupWithForm, Input}
+export default PopupWithForm
